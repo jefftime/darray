6600c9daaeefb2df7c17d067459b1d6b09d111c1
Jeffery Stager <jffry09@gmail.com>
2019-06-16 11:17:22 -0400

    dalen returns size_t and fixed dapusht

--- src/darray.h
+++ src/darray.h
@@ -19,16 +19,18 @@
 #ifndef DARRAY_H
 #define DARRAY_H
 
+#include <stddef.h>
+
 struct darray;
 
-#define dapusht(d, t) dapushn(d, sizeof(t), &(t));
+#define dapusht(d, t) dapushn(d, sizeof(t), (unsigned char *) &(t));
 
 struct darray *danew(unsigned long cap);
 void dafree(struct darray *d);
 unsigned char *dapeel(struct darray *d);
 int dapush(struct darray *d, unsigned char byte);
 int dapushn(struct darray *d, unsigned long len, unsigned char *byte);
-unsigned long dalen(struct darray *d);
+size_t dalen(struct darray *d);
 unsigned char *daptr(struct darray *d);
 
 #endif
